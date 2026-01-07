d6f00873657cdc6fb60dc9a6e7383a4510bb8a13
denytys <dpprihardiningtyas@gmail.com>
2026-01-07 23:25:23 +0700

    update layout

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,20 +1,53 @@
-export default function MenuItem({ icon: Icon, label, url }) {
+export default function MenuItem({
+  icon: Icon,
+  colorIcon: ColorIcon,
+  label,
+  url,
+}) {
   return (
     <div
       onClick={() => window.open(url, "_blank", "noopener,noreferrer")}
       className="cursor-pointer"
     >
-      {/* ZOOM GROUP */}
       <div
-        className="flex flex-col items-center gap-2 transition-all duration-300
+        className="group flex flex-col items-center gap-2 transition-all duration-300
         ease-[cubic-bezier(0.22,1,0.36,1)] hover:scale-115"
       >
         {/* CARD */}
         <div
-          className="w-32.5 h-32.5 rounded-[28px] bg-white/70 flex items-center justify-center 
-          shadow-sm transition-all duration-300 hover:shadow-lg"
+          className="w-26 h-26 rounded-[28px] bg-white/30 flex items-center justify-center 
+          shadow-sm transition-all duration-300 hover:shadow-lg hover:bg-blue-600/30"
         >
-          <Icon size={56} strokeWidth={1.5} className="text-slate-700" />
+          {/* ICON WRAPPER */}
+          <div className="relative w-11.5 h-11.5">
+            {/* LUCIDE ICON */}
+            <Icon
+              size={46}
+              strokeWidth={1.5}
+              className="absolute inset-0 text-slate-700 transition-all duration-500
+              ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-75
+              group-hover:opacity-0 z-10"
+            />
+
+            {/* COLOR ICON (FC atau IMAGE) */}
+            {ColorIcon &&
+              (typeof ColorIcon === "string" ? (
+                <img
+                  src={ColorIcon}
+                  alt={label}
+                  className="absolute inset-0 w-full h-full object-contain opacity-0 scale-75
+                  transition-all duration-900 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:opacity-100
+                  group-hover:scale-130 z-0 pointer-events-none"
+                />
+              ) : (
+                <ColorIcon
+                  size={46}
+                  className="absolute inset-0 opacity-0 scale-75 transition-all duration-900
+                  ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:opacity-100 group-hover:scale-120
+                  z-0 pointer-events-none"
+                />
+              ))}
+          </div>
         </div>
 
         {/* LABEL */}
--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -1,14 +1,30 @@
 import {
-  LayoutGrid,
   Settings,
-  Monitor,
   Clock,
   FileText,
-  Bell,
-  ShieldCheck,
-  ClipboardCheck,
+  Megaphone,
+  FileSearchCorner,
+  QrCode,
+  SmilePlus,
+  LayoutDashboard,
+  GitCompare,
+  ChartNoAxesColumn,
 } from "lucide-react";
 
+import {
+  FcSettings,
+  FcAdvertising,
+  FcDocument,
+  FcFinePrint,
+  FcFolder,
+  FcApprove,
+} from "react-icons/fc";
+
+import iconBT from "../assets/images/iconbt.png";
+import iconPS from "../assets/images/iconpresensi.png";
+import iconPQ from "../assets/images/iconpassq.png";
+import iconES from "../assets/images/iconesps.png";
+import iconWM from "../assets/images/iconwebmon.png";
 import MenuItem from "../components/MenuItem";
 import logo from "../assets/images/barantin-L-BW.png";
 
@@ -28,51 +44,108 @@ export default function Portal() {
       {/* CONTENT */}
       <main className="flex-1 flex flex-wrap justify-center items-center gap-8 my-6">
         {/* SIDEBAR 1 */}
-        <div className="grid grid-cols-2 gap-6 p-6 bg-white/30 backdrop-blur-md rounded-[60px] shadow-md">
-          <MenuItem
-            icon={LayoutGrid}
-            label="Best Trust"
-            url="https://apps.karantinaindonesia.go.id/"
-          />
-          <MenuItem
-            icon={Settings}
-            label="Sys-Admin"
-            url="https://apps.karantinaindonesia.go.id/sys-admin/"
-          />
-          <MenuItem
-            icon={Monitor}
-            label="Web Mon"
-            url="https://intranet.karantinaindonesia.go.id/webmon/"
-          />
-          <MenuItem
-            icon={Clock}
-            label="ePresensi"
-            url="https://presensi.karantinaindonesia.go.id/"
-          />
+        <div className="relative">
+          <div className="absolute -top-5 left-8 z-10">
+            <div className="px-6 py-2 bg-white/40 backdrop-blur-md rounded-full text-sm font-semibold text-gray-700 shadow-md">
+              Admin
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3 gap-6 p-6 pt-10 bg-white/30 backdrop-blur-md rounded-[60px] shadow-md">
+            <MenuItem
+              icon={LayoutDashboard}
+              colorIcon={iconBT}
+              label="Best Trust"
+              url="https://apps.karantinaindonesia.go.id/"
+            />
+
+            <MenuItem
+              icon={Settings}
+              colorIcon={FcSettings}
+              label="Sys-Admin"
+              url="https://apps.karantinaindonesia.go.id/sys-admin/"
+            />
+
+            <MenuItem
+              icon={ChartNoAxesColumn}
+              colorIcon={iconWM}
+              label="Web Mon"
+              url="https://intranet.karantinaindonesia.go.id/webmon/"
+            />
+
+            <MenuItem
+              icon={Clock}
+              colorIcon={iconPS}
+              label="ePresensi"
+              url="https://presensi.karantinaindonesia.go.id/"
+            />
+
+            <MenuItem
+              icon={GitCompare}
+              colorIcon={iconES}
+              label="ESPS"
+              url="https://intranet.karantinaindonesia.go.id/esps/"
+            />
+          </div>
         </div>
 
         {/* SIDEBAR 2 */}
-        <div className="grid grid-cols-3 gap-6 p-6 bg-white/30 backdrop-blur-md rounded-[60px] shadow-md">
-          <MenuItem
-            icon={FileText}
-            label="PTK Online"
-            url="https://ptk.karantinaindonesia.go.id/"
-          />
-          <MenuItem
-            icon={Bell}
-            label="Prior Notice"
-            url="https://notice.karantinaindonesia.go.id/"
-          />
-          <MenuItem
-            icon={ShieldCheck}
-            label="Check Cert"
-            url="https://ptk.karantinaindonesia.go.id/check-cert/"
-          />
-          <MenuItem
-            icon={ClipboardCheck}
-            label="PassQ"
-            url="https://passq.karantinaindonesia.go.id/officer/"
-          />
+        <div className="relative">
+          <div className="absolute -top-5 left-8 z-10">
+            <div className="px-6 py-2 bg-white/40 backdrop-blur-md rounded-full text-sm font-semibold text-gray-700 shadow-md">
+              Pengguna Jasa
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3 gap-6 p-6 pt-10 bg-white/30 backdrop-blur-md rounded-[60px] shadow-md">
+            <MenuItem
+              icon={SmilePlus}
+              colorIcon={FcApprove}
+              label="Registrasi"
+              url="https://registrasi.karantinaindonesia.go.id/"
+            />
+
+            <MenuItem
+              icon={FileText}
+              colorIcon={FcDocument}
+              label="PTK Online"
+              url="https://ptk.karantinaindonesia.go.id/"
+            />
+
+            <MenuItem
+              icon={Megaphone}
+              colorIcon={FcAdvertising}
+              label="Prior Notice"
+              url="https://notice.karantinaindonesia.go.id/"
+            />
+
+            <MenuItem
+              icon={FileSearchCorner}
+              colorIcon={FcFinePrint}
+              label="Check Cert"
+              url="https://ptk.karantinaindonesia.go.id/check-cert/"
+            />
+
+            <MenuItem
+              icon={QrCode}
+              colorIcon={iconPQ}
+              label="PassQ"
+              url="https://passq.karantinaindonesia.go.id/officer/"
+            />
+          </div>
+        </div>
+
+        {/* SIDEBAR 3 */}
+        <div className="relative">
+          <div className="absolute -top-5 left-8 z-10">
+            <div className="px-6 py-2 bg-white/40 backdrop-blur-md rounded-full text-sm font-semibold text-gray-700 shadow-md">
+              Data
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3 gap-6 p-6 pt-10 w-100 h-86 bg-white/30 backdrop-blur-md rounded-[60px] shadow-md min-h-50">
+            {/* isi nanti */}
+          </div>
         </div>
       </main>
 
